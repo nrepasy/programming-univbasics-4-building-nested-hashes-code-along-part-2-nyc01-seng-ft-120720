0c794242553fdab4d80909077e3f9fda7b0857fb
nick repasy <nrepasy@gmail.com>
2020-11-28 03:39:38 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -44,10 +44,9 @@ def add_characters
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}]
       }
-      :additional_characters => [
+      :additional_characters =>
         {:name "Prince Escalus"}
         {:name "Apothecary"}
-        ]
         
       }
    }
